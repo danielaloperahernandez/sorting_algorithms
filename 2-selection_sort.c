fa0424385995fd863ae85ce966c7d1690cc818d4
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-11 18:16:04 +0000

    fix function selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,16 +10,16 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = &array[i];
+		min = array + i;
 		for (j = i + 1; j < size; j++)
-			min = (array[j] < *min) ? (array[j]) : min;
+			min = (array[j] < *min) ? (array + j) : min;
 
-		if (array[i] != *min)
+		if ((array + i) != min)
 		{
 			tmp = array[i];
 			array[i] = *min;
 			*min = tmp;
+			print_array(array, size);
 		}
-		print_array(array, size);
 	}
 }
