03f3875dbe5df0f5ab1c2eae7024b1a8696a1a08
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-12 02:24:02 +0000

    update functions and fix conver spaces

--- sort.h
+++ sort.h
@@ -1,5 +1,7 @@
 #ifndef _SORT_H_
 #define _SORT_H_
+
+/*Librarys standars*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -18,15 +20,26 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
+/*Prints an array and list of integers*/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/*Function that sorts an array of integers in ascending order*/
 void bubble_sort(int *array, size_t size);
+
+/*Function that sorts a doubly linked list of integers in ascending order*/
 void insertion_sort_list(listint_t **list);
+
+/*Function that sorts an array of integers in ascending order using*/
 void selection_sort(int *array, size_t size);
+
+/*Function that sorts an array of integers in ascending order using*/
 void quick_sort(int *array, size_t size);
+
+/*Function that sorts an array of integers in ascending order*/
 void shell_sort(int *array, size_t size);
+
+/*Function that sorts a doubly linked list of integers in ascending*/
 void cocktail_sort_list(listint_t **list);
 
 #endif /* !_SORT_H_*/
