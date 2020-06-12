4defb0f5a18f6af5f44fad72ad2f76cc402e786b
daniela <1469@holbertonschool.com>
2020-06-12 21:49:10 +0000

    Fixing 103 task

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,71 +1,37 @@
 #include "sort.h"
 
-void merge_swap(int *new_array, int *array, size_t left, size_t right)
+void merge_swap(int *new_array, int *array, size_t middle, size_t left, size_t right)
 {
-    size_t middle = (left + right) / 2;
-    size_t i = 0, j = 0, index;
+    size_t i, j, index = 0;
 
     printf("Merging...\n");
     printf("[left]: ");
     print_array(array + left, middle - left);
-    printf("[rigth]: ");
-    print_array(array + right, right - middle);
+    printf("[right]: ");
+    print_array(array + middle, right - middle);
 
-    for ( ; i <= left && j <= right; )
-    {
-        if (array[i] <= array[j])
-        {
-            new_array[index] = array[i];
-            i++;
-        }
-        else
-        {
-            new_array[index] = array[j];
-            j++;
-        }
-        index++;
-    }
-    while (i < left)
-    {
-        new_array[index] = array[i];
-        index++;
-        i++;
-    }
-    while (j < right)
-    {
-        new_array[index] = array[j];
-        index++;
-        j++;
-    }
-    /*for (i = left; index < right; index++)
-    {
-        if (i < middle && (j >= right || array[i] <= array[j]))
-        {
-            new_array[index] = array[i];
-            i++;
-        }
-        else
-        {
-            new_array[index] = array[j];
-            j++;
-        }
-    } */
+    for (i = left, j = middle; i < middle && j < right; index++)
+        new_array[index] = (array[i] < array[j]) ? array[i++] : array[j++];
+    for(; i < middle; i++)
+        new_array[index++] = array[i];
+    for(; j < right; j++)
+        new_array[index++] = array[j];
+    for(i = left, index = 0; i < right; i++)
+        array[i] = new_array[index++];
 
-    for (i = 0; new_array; i++)
-        array[i] = new_array[i];
     printf("[Done]: ");
-    print_array(array, right - left);
-    
+    print_array(array + left, right - left);
+
 }
 void merge_recursion(int *new_array, int *array, size_t left, size_t right)
 {
     size_t middle = (left + right) / 2;
 
     if ((right - left) < 2)
-        return;  
+        return;
     merge_recursion(new_array, array, left, middle);
-    merge_recursion(new_array, array, middle + 1, right);
-    merge_swap(new_array, array, left, right);
+    merge_recursion(new_array, array, middle, right);
+    merge_swap(new_array, array, middle, left, right);
 }
 
 
@@ -75,10 +41,10 @@ void merge_sort(int *array, size_t size)
 
     if (!array || size < 2)
         return;
-    
+
     new_array = malloc(sizeof(int) * size);
     if (!new_array)
         return;
 
-    merge_recursion(new_array, array, 0, size); 
+    merge_recursion(new_array, array, 0, size);
 }
